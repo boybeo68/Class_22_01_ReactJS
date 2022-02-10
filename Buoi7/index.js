32443a33abf68dbfd93dd3895a9373e6a0528fcd
Tung, Nguyen Thac <tung.nguyen.thac@ntq-solution.com.vn>
2022-02-10 20:29:44 +0700

    first buoi 7

---
+++ Buoi7/index.js
@@ -0,0 +1,57 @@
+// console.log("hello class external")
+// Cách học 1 ngôn ngữ lập trình. 
+// - Khai báo biến,
+// - Các kiểu dữ liệu của ngôn ngữ đó 
+// - Các hàm xử lý logic mà ngôn ngữ hỗ trợ
+// + Chung: các toán tử: + - * / ,...  if, else, vòng lặp for
+
+
+
+// + Ngôn ngữ đó có hàm xử lý gì đặc biệt không? Array, Object 
+// + Cái khó nhất nhất của ngôn ngữ này là gì. 
+
+
+// + Thực hành làm side project. 
+
+
+var a = 10;
+a = 20;
+
+let b = 10;
+b = 20;
+
+console.log(a);
+console.log(b);
+const PI = 3.14;
+let number = 13;
+let string1 = "Tôi là Tùng 1"
+let string2 = 'Tôi là Tùng 2'
+let string3 = `Tôi là Tùng 3`
+
+console.log(string3);
+let boolean = true;
+let car = {
+    color: 'red',
+    price: 10000,
+    own: "Tung",
+    isBuy: false
+}
+let people = {
+    name: "Trình",
+    age: 20,
+    garduate: true
+}
+
+let array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] // mảng
+let students = ["Trần Tiến", "Xuân Hiệp", "Mạnh Cường"];
+
+function numberGet() {
+    return 5
+}
+let notValue = null
+// hiển thị  học sinh được 30 điểm đầu tiên thi đại học.
+let studentPass = null;
+// studentPass = "Xuân Hiệp"
+console.log(typeof studentPass);
+// 3 cách khai báo biến: var, const, let
+// Các kiểu dữ liệu trong JS: 
