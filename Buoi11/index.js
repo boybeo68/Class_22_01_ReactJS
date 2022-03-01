00c2cbee2de684dedcad344f6fcdc76412ce69af
nguyen thac tung <tung.nguyen.thac@ntq-solution.com.vn>
2022-03-01 23:10:34 +0700

    done code buoi11

--- Buoi11/index.js
+++ Buoi11/index.js
@@ -1,4 +1,8 @@
 const shortid = require("shortid");
+var readlineSync = require("readline-sync");
+// Wait for user's response.
+var userName = readlineSync.question("May I have your name? ");
+console.log("Hi " + userName + "!");
 // Object
 let people = {
   name: "Tung",
