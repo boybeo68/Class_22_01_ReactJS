ffee6f9ccba686a35b1038f118086a85393e1592
nguyen thac tung <tung.nguyen.thac@ntq-solution.com.vn>
2022-03-29 22:03:18 +0700

    btvn

--- Buoi17/my-app/src/randomGame/Header.js
+++ Buoi17/my-app/src/randomGame/Header.js
@@ -10,7 +10,9 @@ export default function Header(props) {
       <p>
         Tôi đã chọn một số random trong khoảng 1 đến 100, bạn có thể đoán được?
       </p>
-      <p>{props.randomNumberKey}</p>
+      {/* <p>{props.randomNumberKey}</p> */}
     </div>
   );
 }
+
+// làm lại random game -> mục tiêu tự hoàn thành được mà ko cần xem code
