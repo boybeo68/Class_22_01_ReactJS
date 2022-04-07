cb6a9313457d3bcded95e091166075c3727ce263
nguyen thac tung <tung.nguyen.thac@ntq-solution.com.vn>
2022-04-07 21:43:46 +0700

    add product

--- Buoi20/connect-api/src/components/ListProducts.js
+++ Buoi20/connect-api/src/components/ListProducts.js
@@ -3,25 +3,79 @@ import axios from "axios";
 
 export default function ListProducts() {
   const [listProduct, setlistProduct] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setdescription] = useState("");
+  const [price, setPrice] = useState("");
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    let result = await axios.get(
-      "https://624ede528c5bf4a10542cebf.mockapi.io/products"
-    );
-    console.log(result);
-    setlistProduct(result.data);
+    try {
+      let result = await axios.get(
+        "https://624ede528c5bf4a10542cebf.mockapi.io/products"
+      );
+      setlistProduct(result.data);
+    } catch (error) {
+      alert("loi api");
+    }
   };
 
   const renderListProduct = (params) => {
     if (listProduct) {
       return listProduct.map((item, index) => {
-        return <p key={index}>{item.name}</p>;
+        return (
+          <div className="list-wrap">
+            <p>{item.name}</p>
+            <div className="detail">
+              <p>{item.description}</p>
+              <p>{item.price}</p>
+            </div>
+          </div>
+        );
       });
     }
   };
 
-  return <div>{renderListProduct()}</div>;
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const onChangeDescription = (e) => {
+    setdescription(e.target.value);
+  };
+  const onChangePrice = (e) => {
+    setPrice(e.target.value);
+  };
+  const addProduct = async () => {
+    try {
+      console.log({ name, description, price });
+      await axios.post("https://624ede528c5bf4a10542cebf.mockapi.io/products", {
+        name: name,
+        image: "https://loremflickr.com/320/240",
+        description: description,
+        price: price,
+      });
+      setName("");
+      setPrice("");
+      setdescription("");
+      fetchData();
+    } catch (error) {
+      alert("loi api");
+    }
+  };
+
+  return (
+    <div>
+      <div>{renderListProduct()}</div>
+      <div className="formData">
+        <label htmlFor="">name</label>
+        <input value={name} type="text" onChange={onChangeName} />
+        <label htmlFor="">price</label>
+        <input value={price} type="number" onChange={onChangePrice} />
+        <label htmlFor="">description</label>
+        <input value={description} type="text" onChange={onChangeDescription} />
+        <button onClick={addProduct}>add product</button>
+      </div>
+    </div>
+  );
 }
