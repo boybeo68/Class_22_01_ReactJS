11dd12a936ad52be34f596babc5ce9890e100e5b
nguyen thac tung <tung.nguyen.thac@ntq-solution.com.vn>
2022-04-19 21:46:41 +0700

    sử dụng clone data để làm logic trên slice

--- Buoi23/redux-appt3h/src/features/posts/postSlice.js
+++ Buoi23/redux-appt3h/src/features/posts/postSlice.js
@@ -2,11 +2,18 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = [
   {
-    postTitle: "Bata default",
+    postTitle: "Bata default20",
+    price: 20,
     postId: 1,
   },
   {
-    postTitle: "Aata default 2",
+    postTitle: "Aata default 50",
+    price: 50,
+    postId: 2,
+  },
+  {
+    postTitle: "Aata default 30",
+    price: 30,
     postId: 2,
   },
 ];
@@ -16,16 +23,25 @@ export const postSlice = createSlice({
   initialState,
   reducers: {
     addPost: (state, action) => {
-      state.push({
-        postTitle: action.payload,
-        postId: nanoid(),
-      });
-      // state.push(action.payload);
+      // state.push({
+      //   postTitle: action.payload,
+      //   postId: nanoid(),
+      // });
+
+      let cloneState = JSON.parse(JSON.stringify(state));
+      return [
+        ...cloneState,
+        { postTitle: action.payload, postId: nanoid(), price: 10 },
+      ];
     },
     sortPost: (state, action) => {
-      // state.sort((a, b) => b.name - a.name);
+      let cloneState = [...state];
+      cloneState.sort((a, b) => a.price - b.price);
+      return cloneState;
     },
   },
 });
+// nếu thay đổi state gốc không cần return
+// Nếu clone data thì cần return ra giá trị state mới
 export const { addPost, sortPost } = postSlice.actions;
 export default postSlice.reducer;
