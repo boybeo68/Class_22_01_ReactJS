f0c42f7ffcb8c781e043084bc5ba29696f15e69e
nguyen thac tung <tung.nguyen.thac@ntq-solution.com.vn>
2022-04-21 20:09:51 +0700

    call api with redux

--- Buoi23/redux-appt3h/src/features/posts/Post.js
+++ Buoi23/redux-appt3h/src/features/posts/Post.js
@@ -1,20 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Post.module.css";
 import { useSelector, useDispatch } from "react-redux";
-import { addPost, sortPost } from "./postSlice";
+import { addPost, addTodo, getListTodo, sortPost } from "./postSlice";
 
 // dispatch action
 export default function Post() {
   const [inputValue, setInputValue] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const dispatch = useDispatch();
-  const dataPost = useSelector((state) => state.postReducer);
-  const dataCount = useSelector((state) => state.counter.value);
+  const dataPost = useSelector((state) => state?.postReducer?.data);
+  const dataCount = useSelector((state) => state?.counter?.value);
+  useEffect(() => {
+    dispatch(getListTodo());
+  }, []);
 
   return (
     <div>
       <h1 className={styles.title}>Title Post</h1>
+      <input
+        value={title}
+        placeholder={"title"}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+        type="text"
+      />
+      <input
+        value={content}
+        placeholder={"content"}
+        onChange={(e) => {
+          setContent(e.target.value);
+        }}
+        type="text"
+      />
+      <button
+        onClick={async () => {
+          await dispatch(addTodo({ content, title })).unwrap();
+          dispatch(getListTodo());
+        }}
+      >
+        Add Todo
+      </button>
+
       {dataPost.map((item, index) => {
-        return <p key={index}>{item.postTitle}</p>;
+        return <p key={index}>{item.content}</p>;
       })}
       <p>{dataCount}</p>
       <input
@@ -43,3 +73,8 @@ export default function Post() {
 }
 
 // read/write
+
+// async function.
+
+// Trước đây : call api  -> lưu dữ liệu trong state
+// now: dispatch action (call api  -> lưu dữ liệu trong redux )
--- Buoi23/redux-appt3h/src/features/posts/postSlice.js
+++ Buoi23/redux-appt3h/src/features/posts/postSlice.js
@@ -1,44 +1,92 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-  {
-    postTitle: "Bata default20",
-    price: 20,
-    postId: 1,
+const initialState = {
+  data: [],
+  loading: false,
+  error: null,
+};
+
+export const getListTodo = createAsyncThunk("post/getListTodo", async () => {
+  try {
+    const res = await axios.get(
+      "https://61b02a623e2aba0017c49636.mockapi.io/Todo",
+    );
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    console.log("co loi xay ra ", error);
+  }
+});
+
+export const addTodo = createAsyncThunk(
+  "post/addTodo",
+  async ({ content, title }) => {
+    try {
+      const res = await axios.post(
+        "https://61b02a623e2aba0017c49636.mockapi.io/Todo",
+        {
+          title,
+          content,
+        },
+      );
+      console.log(res.data);
+      return res.data;
+    } catch (error) {
+      return "Có lỗi rồi";
+    }
   },
-  {
-    postTitle: "Aata default 50",
-    price: 50,
-    postId: 2,
-  },
-  {
-    postTitle: "Aata default 30",
-    price: 30,
-    postId: 2,
-  },
-];
+);
 
 export const postSlice = createSlice({
   name: "post",
   initialState,
   reducers: {
-    addPost: (state, action) => {
-      // state.push({
-      //   postTitle: action.payload,
-      //   postId: nanoid(),
-      // });
+    // addPost: (state, action) => {
+    //   // C1:
+    //   // state.push({
+    //   //   postTitle: action.payload,
+    //   //   postId: nanoid(),
+    //   // });
+    //   // C2
+    //   let cloneState = JSON.parse(JSON.stringify(state));
+    //   return [
+    //     ...cloneState,
+    //     { postTitle: action.payload, postId: nanoid(), price: 10 },
+    //   ];
+    // },
+    // sortPost: (state, action) => {
+    //   let cloneState = [...state];
+    //   cloneState.sort((a, b) => a.price - b.price);
+    //   return cloneState;
+    // },
+  },
 
-      let cloneState = JSON.parse(JSON.stringify(state));
-      return [
-        ...cloneState,
-        { postTitle: action.payload, postId: nanoid(), price: 10 },
-      ];
-    },
-    sortPost: (state, action) => {
-      let cloneState = [...state];
-      cloneState.sort((a, b) => a.price - b.price);
-      return cloneState;
-    },
+  extraReducers(builder) {
+    builder
+      // getlist todo
+      .addCase(getListTodo.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(getListTodo.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      })
+      .addCase(getListTodo.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      // add todo
+      .addCase(addTodo.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(addTodo.fulfilled, (state, action) => {
+        state.loading = false;
+        // state.data = action.payload;
+      })
+      .addCase(addTodo.rejected, (state, action) => {
+        state.loading = false;
+      });
   },
 });
 // nếu thay đổi state gốc không cần return
@@ -47,5 +95,7 @@ export const { addPost, sortPost } = postSlice.actions;
 export default postSlice.reducer;
 // npx create-react-app redux-appt3h --template redux
 
-// btvn: làm đầy đủ todo list = redux
-
+// khi dispatch action async (call api ) => redux chia thành 3 action : pending , fullfill, reject
+// pending: trạng thái chờ.
+// fullfill: thành công.
+// reject: thất bái
